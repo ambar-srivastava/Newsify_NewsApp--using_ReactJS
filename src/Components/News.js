a721ba796f50e7e6e05602430aa691a7717997ba
Ambar Srivastava <67692421+ambar-srivastava@users.noreply.github.com>
2021-09-03 01:55:44 +0530

    Added loading spinner & variable pageSize

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,39 +1,76 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from 'react';
+import NewsItem from './NewsItem';
+import Spinner from './Spinner';
 
 export class News extends Component {
 
     constructor() {
         super();
-        console.log("I'm a constructor from news component");
         this.state = {
             articles: [],
-            loading: false
+            loading: false,
+            page: 1
         }
     }
 
-    async componentDidMount(){
-        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=c8365f61d1dd4087a536a64e213a6982";
+    async componentDidMount() {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=c8365f61d1dd4087a536a64e213a6982&page=1&pageSize=${this.props.pageSize}`;
+        this.setState({loading: true});
         let data = await fetch(url);
         let parsedData = await data.json()
-        this.setState({articles: parsedData.articles})
+            this.setState({ articles: parsedData.articles, 
+                totalResults: parsedData.totalResults, 
+                loading: false })
+    }
+
+    handlePrevClick = async () => {
+        console.log("Previous");
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=c8365f61d1dd4087a536a64e213a6982&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        this.setState({loading: true});
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles,
+            loading: false
+        })
+    }
+
+    handleNextClick = async () => {
+        console.log("Next");
+        if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=c8365f61d1dd4087a536a64e213a6982&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+            let data = await fetch(url);
+            this.setState({loading: true});
+            let parsedData = await data.json();
+            this.setState({
+                page: this.state.page + 1,
+                articles: parsedData.articles,
+                loading: false
+            })
+        }
     }
 
     render() {
         return (
-            <div className="container my-3">
-                <h2>Newsify - Top Headlines</h2>
+            <div className="container my-4">
+                <h1 className="text-center">Newsify - Top Headlines</h1>
+                {this.state.loading && <Spinner/>}
                 <div className="row">
-                    {this.state.articles.map((element) => {
+                    {!this.state.loading && this.state.articles.map((element) => {
                         return <div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title?element.title.slice(0, 30):""} description={element.description?element.description.slice(0, 60):""} imageUrl={element.urlToImage} newsUrl={element.url} />
+                            <NewsItem title={element.title ? element.title.slice(0, 30) : ""} description={element.description ? element.description.slice(0, 60) : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
                         </div>
                     })}
-                </div>   
+                </div>
+                <div className="container d-flex justify-content-between">
+                    <button disabled={this.state.page <= 1} type="button" className="btn btn-sm btn-success" onClick={this.handlePrevClick}>&larr; Prev</button>
+                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-sm btn-success" onClick={this.handleNextClick}>Next &rarr;</button>
+                </div>
             </div>
+
         )
     }
 }
 
 export default News
-     
